94defa345d3fb56b754b5157ece13ec5923d1a5a
Emma Leonardi - emma.leonardi2@studio.unibo.it <emma.leonardi2@studio.unibo.it>
2021-10-25 11:32:06 +0200

    Minor changes to formatting

--- src/it/unibo/oop/lab05/ex1/UseSet.java
+++ src/it/unibo/oop/lab05/ex1/UseSet.java
@@ -15,54 +15,45 @@ public final class UseSet {
     }
 
     /**
-     * @param args
-     *            ignored
+     * @param args ignored
      */
     public static void main(final String[] args) {
-        /*
-         * Considering the content of "UseCollection, write a program which, in
-         * order:
-         * 
-         * 1) Builds a TreeSet containing Strings
-         * 
-         * 2) Populates such Collection with all the Strings ranging from "1" to
-         * "20"
-         * 
-         * 3) Prints its content
-         * 
-         * 4) Removes all those strings whose represented number is divisible by
-         * three
-         * 
-         * 5) Prints the content of the Set using a for-each costruct
-         * 
-         * 6) Verifies if all the numbers left in the set are even
-         */
-    	final Collection<String> coll=new TreeSet<>();
-    	for(int i=1;i<21;i++) {
-    		coll.add(Integer.toString(i));
-    	}
-    	System.out.println(coll.toString());
-    	final Iterator<String> it=coll.iterator();
-    	
-    	while(it.hasNext()){
-    		if(Integer.parseInt(it.next())%3==0) {
-    			it.remove();
-    		}
-    	}
-    	boolean allEven=true;
-    	
-    	System.out.print("| ");
-    	for(String elem: coll) {
-    		System.out.print(elem+" | ");
-    		if(Integer.parseInt(elem)%2==1) {
-    			allEven=false;
-    		}
-    	}
-    	System.out.println();
-    	System.out.println("Are all the remaining elements even? "+allEven);
-    	
-    	
-    	
-    	
+	/*
+	 * Considering the content of "UseCollection, write a program which, in order:
+	 * 
+	 * 1) Builds a TreeSet containing Strings
+	 * 
+	 * 2) Populates such Collection with all the Strings ranging from "1" to "20"
+	 * 
+	 * 3) Prints its content
+	 * 
+	 * 4) Removes all those strings whose represented number is divisible by three
+	 * 
+	 * 5) Prints the content of the Set using a for-each costruct
+	 * 
+	 * 6) Verifies if all the numbers left in the set are even
+	 */
+	final Collection<String> coll = new TreeSet<>();
+	for (int i = 1; i < 21; i++) {
+	    coll.add(Integer.toString(i));
+	}
+	System.out.println(coll.toString());
+	final Iterator<String> it = coll.iterator();
+	while (it.hasNext()) {
+	    if (Integer.parseInt(it.next()) % 3 == 0) {
+		it.remove();
+	    }
+	}
+	boolean allEven = true;
+
+	System.out.print("| ");
+	for (String elem : coll) {
+	    System.out.print(elem + " | ");
+	    if (Integer.parseInt(elem) % 2 == 1) {
+		allEven = false;
+	    }
+	}
+	System.out.println();
+	System.out.println("Are all the remaining elements even? " + allEven);
     }
 }
