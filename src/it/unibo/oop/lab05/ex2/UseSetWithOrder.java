c2c1946c501998a0bf84230fa413c4f47762fa4e
Emma Leonardi - emma.leonardi2@studio.unibo.it <emma.leonardi2@studio.unibo.it>
2021-10-25 11:32:47 +0200

    Finished second exercise

--- src/it/unibo/oop/lab05/ex2/UseSetWithOrder.java
+++ src/it/unibo/oop/lab05/ex2/UseSetWithOrder.java
@@ -1,5 +1,8 @@
 package it.unibo.oop.lab05.ex2;
 
+import java.util.Collection;
+import java.util.TreeSet;
+
 /**
  * 
  */
@@ -9,20 +12,28 @@ public final class UseSetWithOrder {
     }
 
     /**
-     * @param args
-     *            ignored
+     * @param args ignored
      */
     public static void main(final String[] args) {
-        /*
-         * Write a program which:
-         * 
-         * 1) Creates a new ORDERED TreeSet of Strings. To order the set, define
-         * a new Comparator in a separate class.
-         * 
-         * 2) Inserts in the set a hundred strings containing randomly generated
-         * numbers (use Math.random())
-         * 
-         * 3) Prints the set, which must be ordered
-         */
+
+	/*
+	 * Write a program which:
+	 * 
+	 * 1) Creates a new ORDERED TreeSet of Strings. To order the set, define a new
+	 * Comparator in a separate class.
+	 * 
+	 * 2) Inserts in the set a hundred strings containing randomly generated numbers
+	 * (use Math.random())
+	 * 
+	 * 3) Prints the set, which must be ordered
+	 */
+
+	final int NUM = 100;
+	final Collection<String> coll = new TreeSet<>();
+	for (int i = 0; i < NUM; i++) {
+	    coll.add(Double.toString(Math.random()));
+	}
+	System.out.println(coll.toString());
+
     }
 }
