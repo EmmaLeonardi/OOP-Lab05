126721a454df4cecee4a8b9e98e7606de998d11a
Emma Leonardi <eleonardi.mobile@gmail.com>
2021-11-07 14:37:57 +0100

    Added comment

--- src/it/unibo/oop/lab05/ex2/StringComparator.java
+++ src/it/unibo/oop/lab05/ex2/StringComparator.java
@@ -4,9 +4,19 @@ import java.util.Comparator;
 
 public class StringComparator implements Comparator<String> {
 
-    public int compare(String o1, String o2) {
+	/**
+	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+	 * 
+	 * @param o1 the first string to compare, must be parse-able to double
+	 * @param o2 the second string to compare, must be parse-able to double
+	 * 
+	 * @return 1 if o1 > o2, 0 if o2 == o1, -1 otherwise
+	 * 
+	 */
 
-	return Double.compare(Double.parseDouble(o1), Double.parseDouble(o2));
-    }
+	public int compare(String o1, String o2) {
+
+		return Double.compare(Double.parseDouble(o1), Double.parseDouble(o2));
+	}
 
 }
